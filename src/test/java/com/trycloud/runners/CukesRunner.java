c37ee068649dfb470a45dd9fc505b4ea45b7cc49
Yaroslav <ybondarenko111@gmail.com>
2022-11-19 11:12:13 -0800

    Project was created

---
+++ src/test/java/com/trycloud/runners/CukesRunner.java
@@ -0,0 +1,27 @@
+package com.trycloud.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "pretty",
+                "html:target/cucumber-reports.html",
+        "me.jvt.cucumber.report.PrettyReports:target/cucumber",
+                "rerun:target/rerun.text"},
+        features = "src/test/resources/features",
+        glue = "com/trycloud/step_definitions",
+        dryRun = false,
+        tags ="@wip"
+
+)
+public class CukesRunner {
+
+
+    
+
+
+
+}
